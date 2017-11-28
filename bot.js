dd3b8638809e4d4ec2f0a2f4d9d5e59a069ebd17
Samiirosmaan112 <33898442+Samiirosmaan112@users.noreply.github.com>
2017-11-28 02:57:17 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ client.on('message', message => {
 cilent.on('message', message => {
     if (message.content === 'ping'
         message.reply('BONG'
-     }
+  	}
 });
         // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
